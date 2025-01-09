45e59fddd065d42991c2a4b0114dca263a69a7d2
Ivan Santos <ivansantos.ronem@gmail.com>
2025-01-09 16:01:01 -0300

    add node install

--- environment.c
+++ environment.c
@@ -28,6 +28,8 @@ printf("SSH Key\n");
 sleep(3);
 system("cat .ssh/ed_25519.pub");
 sleep(3);
+system("cd");
+sleep(2);
 system("cat .ssh/id_ed25519.pub >> environment-installer/ssh_pub.txt");
 sleep(2);
 printf("keygen file ssh_pub.txt\n");
@@ -67,14 +69,14 @@ system("curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v.0.39.1/install.s
 sleep(3);
 system("source .bashrc");
 
-printf("\tInstall Node 20.18.1\n");
+printf("\tInstall Node v20.18.1\n");
 sleep(3);
-system("nvm install 20.18.1");
+system("nvm install v20.18.1");
 sleep(3);
 
 printf("\tUse Node Version\n");
 sleep(3);
-system("nvm use 20.18.1");
+system("nvm use v20.18.1");
 sleep(3);
 
 printf("\tInstall pnpm\n");
@@ -111,6 +113,10 @@ sleep(3);
 printf("pm2\n");
 sleep(3);
 system("pm2 --version");
+
+printf("\tInstall Nginx\n");
+sleep(3);
+system("apt-get install nginx");
 
     return 0;
 }
