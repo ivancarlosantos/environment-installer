e58682084f2f535ac9c97096e675a1cc20202254
Ivan Santos <ivansantos.ronem@gmail.com>
2025-01-09 15:44:19 -0300

    add node install

--- environment.c
+++ environment.c
@@ -28,7 +28,9 @@ printf("SSH Key\n");
 sleep(3);
 system("cat .ssh/ed_25519.pub");
 sleep(3);
-system("cat .ssh/id_ed25519.pub >> ssh_pub.txt");
+system("cat .ssh/id_ed25519.pub >> environment-installer/ssh_pub.txt");
+sleep(2);
+printf("keygen file ssh_pub.txt\n");
 
 sleep(5);
 
@@ -50,8 +52,65 @@ sleep(5);
 printf("\tInstall Maven\n\n");
 sleep(3);
 system("apt-get install maven");
+sleep(3);
+printf("maven install successfully\n");
 
 sleep(5);
 
-return 0;
+printf("\tNode Install\n");
+sleep(3);
+system("apt-get install nodejs npm");
+
+printf("\tInstall nvm\n");
+sleep(3);
+system("curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v.0.39.1/install.sh | bash");
+sleep(3);
+system("source .bashrc");
+
+printf("\tInstall Node 20.18.1\n");
+sleep(3);
+system("nvm install 20.18.1");
+sleep(3);
+
+printf("\tUse Node Version\n");
+sleep(3);
+system("nvm use 20.18.1");
+sleep(3);
+
+printf("\tInstall pnpm\n");
+sleep(3);
+system("npm install -g pnpm");
+sleep(3);
+
+printf("\tInstall pm2\n");
+sleep(3);
+system("npm install -g pm2");
+sleep(3);
+
+printf("\tVerify versions installed\n");
+sleep(2);
+
+printf("node\n");
+sleep(3);
+
+system("node -v");
+sleep(3);
+
+printf("nvm\n");
+sleep(3);
+
+system("nvm -v");
+sleep(3);
+
+printf("pnpm\n");
+sleep(3);
+
+system("pnpm --version");
+sleep(3);
+
+printf("pm2\n");
+sleep(3);
+system("pm2 --version");
+
+    return 0;
 }
