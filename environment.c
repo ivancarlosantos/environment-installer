0b9ff84de912eb1afae3fadb5f8737a284cd702b
Ivan Santos <ivansantos.ronem@gmail.com>
2025-01-09 13:16:45 -0300

    first commit

---
+++ environment.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+
+printf("\tAtualizando droplet\n\n");
+sleep(4);
+system("apt update -y && apt upgrade -y");
+sleep(3);
+printf("droplet atualizado com sucesso\n\n");
+
+sleep(5);
+
+printf("\tSSH Generate Keygen\n");
+sleep(3);
+
+printf("Generate SSH Key\n");
+sleep(3);
+system("ssh-keygen -t ed25519 -C 'joao@ibglbrasil.com.br' ");
+sleep(3);
+
+printf("Evaluate Key\n");
+sleep(3);
+system("eval "$(ssh-agent -s)" ");
+sleep(3);
+
+printf("Add Key\n");
+sleep(3);
+system("ssh-add .ssh/id_25519");
+sleep(3);
+
+printf("Copy SSH Key\n");
+sleep(3);
+system("cat .ssh/ed_25519.pub");
+sleep(3);
+system("cat .ssh/id_ed25519.pub >> ssh_pub.txt");
+
+sleep(5);
+
+printf("\tJava Installer\n\n");
+sleep(3);
+system("add-apt-repository ppa:linuxuprising/java -y");
+sleep(3);
+
+printf("update and upgrade droplet\n");
+system("apt update -y && apt upgrade -y");
+sleep(3);
+
+printf("\tBind Java Installer version v17\n\n");
+sleep(3);
+system("apt-get install oracle-java17-installer oracle-java17-set-default");
+
+sleep(5);
+
+printf("\tInstall Maven\n\n");
+sleep(3);
+system("apt-get install maven");
+
+sleep(5);
+
+
+printf("\n\n");
+return 0;
+}
---
+++ environment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Environment Installer"
+
+sleep 3
+
+gcc environment.c -o environment
+
+./environment
+
+sleep 3
